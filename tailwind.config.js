9dfb7340a7feb6eeffe24ddb0553214c83f8e045
DelisaRogger <delrog47231@stud.noroff.no>
2024-11-10 16:45:45 +0100

    tcj

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./feed/**/*.{html,js}","./profile/**/*.{html, js}","./index.html","!./node_modules/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
