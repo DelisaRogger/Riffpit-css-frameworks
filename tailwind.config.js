6e1b1dd80b091045782a797ad47d8bee47d7092e
DelisaRogger <delrog47231@stud.noroff.no>
2024-11-12 22:45:05 +0100

    prof

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./feed/**/*.{html,js}","./profile/**/*.{html, js}","./index.html","!./node_modules/**/*"],
+  content: ["./feed/**/*.{html,js}","./profile/**/*.{html,js}","./index.html","!./node_modules/**/*"],
   theme: {
     extend: {},
   },
