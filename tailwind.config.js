fdc20237032baa1e89a3c6ed69528a1ee60dc0d5
DelisaRogger <delrog47231@stud.noroff.no>
2024-11-18 12:05:09 +0100

    news

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  corePlugins: {
-    boxShadow: true
-  },
   content: ["./feed/**/*.{html,js}","./profile/**/*.{html,js}","./index.html","!./node_modules/**/*"],
   theme: {
     extend: {},
